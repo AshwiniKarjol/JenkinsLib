5f465810d9b06be64866fd0f46db987182770440
AshwiniKarjol <50228278+AshwiniKarjol@users.noreply.github.com>
2019-05-25 14:04:16 +0530

    Update Dockerbuild.groovy

--- vars/Dockerbuild.groovy
+++ vars/Dockerbuild.groovy
@@ -3,7 +3,7 @@ def call (String deockerHub, String authcode, String imagePath)
 
 docker.withRegistry("${deockerHub}", "${authcode}") {
 
-       def customImage = docker.build("s${imagePath}:${BUILD_NUMBER}")
+       def customImage = docker.build("${imagePath}:${BUILD_NUMBER}")
 
        customImage.push()
 }
