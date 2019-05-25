ea27f2294b23451465d1fec69854e9ef950dd242
AshwiniKarjol <50228278+AshwiniKarjol@users.noreply.github.com>
2019-05-25 14:17:47 +0530

    Delete Dockerbuild.groovy

--- vars/Dockerbuild.groovy
+++
@@ -1,9 +0,0 @@
-def call (String deockerHub, String authcode, String imagePath)
-{
-
-docker.withRegistry("${deockerHub}", "${authcode}") {
-
-       def customImage = docker.build("${imagePath}:${BUILD_NUMBER}")
-
-       customImage.push()
-}
